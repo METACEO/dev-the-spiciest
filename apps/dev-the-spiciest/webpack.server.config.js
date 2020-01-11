39725cce86e3b171e8125992fcb0b1c30e5bb2a0
James Allen <james.allen@meta.ceo>
2020-01-11 15:30:15 -0600

    feat(initial): updating paths (with moved files)

--- apps/dev-the-spiciest/webpack.server.config.js
+++ apps/dev-the-spiciest/webpack.server.config.js
@@ -7,7 +7,7 @@ module.exports = {
   mode: 'none',
   entry: {
     // This is our Express server for Dynamic universal
-    server: './server.ts'
+    server: './apps/dev-the-spiciest/server.ts'
   },
   externals: {
     './dist/server/main': 'require("./server/main")'
@@ -19,7 +19,7 @@ module.exports = {
   },
   output: {
     // Puts the output at the root of the dist folder
-    path: path.join(__dirname, 'dist'),
+    path: path.join(__dirname, '../../dist/apps/dev-the-spiciest/server'),
     filename: '[name].js'
   },
   module: {
