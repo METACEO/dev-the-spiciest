97c1663836bc194c36298b6898494446e4feba4f
James Allen <james.allen@meta.ceo>
2020-01-12 01:50:42 -0600

    feat(bulma): populating navbar

--- apps/dev-the-spiciest/src/app/components/navigation/navigation.component.ts
+++ apps/dev-the-spiciest/src/app/components/navigation/navigation.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import { Component, ChangeDetectionStrategy } from '@angular/core';
+
+interface NavigationMenuLink {
+  href: string;
+  icon: string;
+  name: string;
+}
 
 @Component({
   selector: 'dev-the-spiciest-navigation',
@@ -6,11 +12,49 @@ import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
   styleUrls: ['./navigation.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class NavigationComponent implements OnInit {
+export class NavigationComponent {
 
-  constructor() { }
+  links: NavigationMenuLink[] = [
+    {
+      href: 'https://dev.to/thespiciestdev',
+      icon: 'fa-dev',
+      name: 'DEV'
+    },
+    {
+      href: 'https://github.com/METACEO',
+      icon: 'fa-github',
+      name: 'GitHub'
+    },
+    {
+      href: 'https://www.linkedin.com/in/thespiciestdev/',
+      icon: 'fa-linkedin',
+      name: 'LinkedIn'
+    },
+    {
+      href: 'https://www.reddit.com/user/TheSpiciestDev',
+      icon: 'fa-reddit',
+      name: 'Reddit'
+    },
+    {
+      href: 'https://twitter.com/TheSpiciestDev',
+      icon: 'fa-twitter',
+      name: 'Twitter'
+    },
+    {
+      href: 'https://news.ycombinator.com/user?id=TheSpiciestDev',
+      icon: 'fa-y-combinator',
+      name: 'YCombinator'
+    },
+    null,
+    {
+      href: 'https://discordapp.com/channels/@TheSpiciestDev',
+      icon: 'fa-discord',
+      name: 'Discord'
+    }
+  ];
+  showMenu = false;
 
-  ngOnInit() {
+  public toggleShowMenu(): void {
+    this.showMenu = !this.showMenu;
   }
-
 }
--- apps/dev-the-spiciest/src/app/components/navigation/navigation.module.ts
+++ apps/dev-the-spiciest/src/app/components/navigation/navigation.module.ts
@@ -1,9 +1,5 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { MatButtonModule } from '@angular/material/button';
-import { MatIconModule } from '@angular/material/icon';
-import { MatMenuModule } from '@angular/material/menu';
-import { MatToolbarModule } from '@angular/material/toolbar';
 
 import { NavigationComponent } from './navigation.component';
 
@@ -12,11 +8,7 @@ import { NavigationComponent } from './navigation.component';
     NavigationComponent
   ],
   imports: [
-    CommonModule,
-    MatButtonModule,
-    MatIconModule,
-    MatMenuModule,
-    MatToolbarModule,
+    CommonModule
   ],
   exports: [
     NavigationComponent
