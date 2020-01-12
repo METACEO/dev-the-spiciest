e4a9ebc84ae674d530e4487a73cb2fd77f479bb1
James Allen <james.allen@meta.ceo>
2020-01-11 23:41:46 -0600

    style(init-routing): format generated code

--- apps/dev-the-spiciest/src/app/pages/home/home.module.ts
+++ apps/dev-the-spiciest/src/app/pages/home/home.module.ts
@@ -1,13 +1,15 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+
 import { HomeComponent } from './home.component';
 
-
-
 @NgModule({
-  declarations: [HomeComponent],
+  declarations: [
+    HomeComponent
+  ],
   imports: [
     CommonModule
   ]
 })
-export class HomeModule { }
+export class HomeModule {
+}
