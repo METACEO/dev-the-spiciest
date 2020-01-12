8f201a3dbfc442a4096ad1ccfaf77d9111d7c8da
James Allen <james.allen@meta.ceo>
2020-01-11 19:55:46 -0600

    dep(material): yarn ng add @angular/material

--- apps/dev-the-spiciest/src/app/app.module.ts
+++ apps/dev-the-spiciest/src/app/app.module.ts
@@ -3,10 +3,11 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule.withServerTransition({ appId: 'serverApp' }), HttpClientModule],
+  imports: [BrowserModule.withServerTransition({ appId: 'serverApp' }), HttpClientModule, BrowserAnimationsModule],
   providers: [],
   bootstrap: [AppComponent]
 })
