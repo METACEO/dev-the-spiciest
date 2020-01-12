8732f0383a7571e0118751490ddbb7f5e9704e3a
James Allen <james.allen@meta.ceo>
2020-01-12 02:07:32 -0600

    feat(navigate-home): initial home button

--- apps/dev-the-spiciest/src/app/components/navigation/navigation.module.ts
+++ apps/dev-the-spiciest/src/app/components/navigation/navigation.module.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 import { NavigationComponent } from './navigation.component';
 
@@ -8,7 +9,8 @@ import { NavigationComponent } from './navigation.component';
     NavigationComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule
   ],
   exports: [
     NavigationComponent
