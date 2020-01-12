a1accabd5479f9347605ba9f613c9582f8c484c3
James Allen <james.allen@meta.ceo>
2020-01-11 23:55:56 -0600

    feat(init-routing): yarn ng generate @schematics/angular:component --name=pages/not-found --project=dev-the-spiciest --style=scss --changeDetection=OnPush

--- apps/dev-the-spiciest/src/app/pages/not-found/not-found.module.ts
+++ apps/dev-the-spiciest/src/app/pages/not-found/not-found.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NotFoundComponent } from './not-found.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [NotFoundComponent],
   imports: [
     CommonModule
   ]
