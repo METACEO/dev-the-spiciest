e5788e7a45086b07ed1e802644945033914d96a4
James Allen <james.allen@meta.ceo>
2020-03-04 20:17:32 -0600

    refactor: example-specific selector name

--- apps/dev-the-spiciest/src/app/pages/examples/dynamically-loading-charts/chart/chart.component.ts
+++ apps/dev-the-spiciest/src/app/pages/examples/dynamically-loading-charts/chart/chart.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
 
 @Component({
-  selector: 'dev-the-spiciest-chart',
+  selector: 'dev-the-spiciest-dynamically-loading-charts-chart',
   templateUrl: './chart.component.html',
   styleUrls: ['./chart.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
