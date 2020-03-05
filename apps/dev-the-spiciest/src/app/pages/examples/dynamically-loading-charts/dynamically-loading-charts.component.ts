3ae578dfae2c462e15f0921752f2479e6a305a98
James Allen <james.allen@meta.ceo>
2020-03-04 20:04:36 -0600

    feat: ng generate @schematics/angular:component --name=pages/examples/dynamically-loading-charts --project=dev-the-spiciest --style=scss --changeDetection=OnPush

---
+++ apps/dev-the-spiciest/src/app/pages/examples/dynamically-loading-charts/dynamically-loading-charts.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+
+@Component({
+  selector: 'dev-the-spiciest-dynamically-loading-charts',
+  templateUrl: './dynamically-loading-charts.component.html',
+  styleUrls: ['./dynamically-loading-charts.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class DynamicallyLoadingChartsComponent implements OnInit {
+
+  constructor() {
+  }
+
+  ngOnInit() {
+  }
+
+}
